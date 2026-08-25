d891898e8bf48dcf69c5d69a06d563e9e5c83604
baskalmoment <123458726+BaskalM0ment@users.noreply.github.com>
2026-08-25 10:22:41 +0200

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,85 +1,60 @@
-const CACHE_NAME = "dino-game-v1";
+const CACHE_NAME = "dino-game-v3";
 
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/sw.js"
+const FILES = [
+    "./",
+    "./index.html"
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
 
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES);
+        })
     );
-
-    self.skipWaiting();
 });
 
-
 self.addEventListener("activate", event => {
-
     event.waitUntil(
         caches.keys().then(keys => {
-
             return Promise.all(
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
             );
-
+        }).then(() => {
+            return self.clients.claim();
         })
     );
-
-    self.clients.claim();
 });
-
 
 self.addEventListener("fetch", event => {
 
-    if (event.request.method !== "GET") {
-        return;
-    }
-
     event.respondWith(
 
-        caches.match(event.request)
-            .then(cachedResponse => {
+        fetch(event.request)
+            .then(response => {
 
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+                const copy =
+                    response.clone();
 
-                return fetch(event.request)
-                    .then(response => {
-
-                        if (
-                            !response ||
-                            response.status !== 200 ||
-                            response.type !== "basic"
-                        ) {
-                            return response;
-                        }
-
-                        const responseClone =
-                            response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(
-                                    event.request,
-                                    responseClone
-                                );
-                            });
-
-                        return response;
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(
+                            event.request,
+                            copy
+                        );
                     });
 
+                return response;
             })
             .catch(() => {
-
-                return caches.match("/index.html");
-
+                return caches.match(
+                    event.request
+                );
             })
+
     );
+
 });
