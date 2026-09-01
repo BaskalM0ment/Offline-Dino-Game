39e76ea81a7be691c27f3c4940c291e00c6e0bb7
baskalmoment <123458726+BaskalM0ment@users.noreply.github.com>
2026-09-01 08:58:17 +0200

    Update service worker cache and event listeners

--- sw.js
+++ sw.js
@@ -1,55 +1,126 @@
-const CACHE_NAME = "dino-game-v5";
+const CACHE_NAME = "dino-runner-v12";
 
-const FILES = [
-    "./",
-    "./index.html"
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/sw.js"
 ];
 
-self.addEventListener("install", event => {
-    self.skipWaiting();
 
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES);
-        })
-    );
-});
+self.addEventListener(
+    "install",
+    event => {
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        }).then(() => {
-            return self.clients.claim();
-        })
-    );
-});
-
-self.addEventListener("fetch", event => {
-
-    event.respondWith(
-        fetch(event.request)
-            .then(response => {
-
-                const copy =
-                    response.clone();
-
-                caches.open(CACHE_NAME)
-                    .then(cache => {
-                        cache.put(
-                            event.request,
-                            copy
-                        );
-                    });
-
-                return response;
-            })
-            .catch(() =>
-                caches.match(event.request)
+        event.waitUntil(
+            caches.open(
+                CACHE_NAME
+            ).then(
+                cache =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
             )
-    );
-});
+        );
+
+        self.skipWaiting();
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys().then(
+                keys => {
+
+                    return Promise.all(
+
+                        keys
+                            .filter(
+                                key =>
+                                    key !==
+                                    CACHE_NAME
+                            )
+                            .map(
+                                key =>
+                                    caches.delete(
+                                        key
+                                    )
+                            )
+
+                    );
+                }
+            )
+        );
+
+        self.clients.claim();
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+            return;
+        }
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            ).then(
+                cached => {
+
+                    if (cached) {
+                        return cached;
+                    }
+
+                    return fetch(
+                        event.request
+                    ).then(
+                        response => {
+
+                            /*
+                                Cache successful
+                                same-origin responses.
+                            */
+
+                            if (
+                                response &&
+                                response.status === 200 &&
+                                response.type ===
+                                    "basic"
+                            ) {
+
+                                const copy =
+                                    response.clone();
+
+                                caches.open(
+                                    CACHE_NAME
+                                ).then(
+                                    cache => {
+
+                                        cache.put(
+                                            event.request,
+                                            copy
+                                        );
+                                    }
+                                );
+                            }
+
+                            return response;
+                        }
+                    );
+                }
+            )
+        );
+    }
+);
